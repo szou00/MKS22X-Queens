e7cbee234e1807f6f294dd2514790213f140fd72
Sharon Zou <szou00@stuy.edu>
2019-02-11 06:54:01 -0500

    added more testing methods

--- driver.java
+++ driver.java
@@ -2,15 +2,24 @@ public class driver {
 
 public static void main(String[] args) {
   QueenBoard queenboard = new QueenBoard(8);
+  QueenBoard queenboard1 = new QueenBoard(8);
+  // System.out.println(queenboard);
+  // queenboard.addQueen(0,0);
+  // System.out.println(queenboard);
+  // queenboard.addQueen(2,0);
+  // System.out.println(queenboard);
+  // queenboard.removeQueen(0,0);
+  // System.out.println(queenboard);
+  // queenboard.addQueen(2,0);
+  // queenboard.addQueen(0,0);
+  System.out.println(queenboard.solve());
   System.out.println(queenboard);
-  queenboard.addQueen(0,0);
+  System.out.println(queenboard.countSolutions());
   System.out.println(queenboard);
-  queenboard.addQueen(2,0);
-  System.out.println(queenboard);
-  queenboard.removeQueen(0,0);
-  System.out.println(queenboard);
-  queenboard.solve();
-  System.out.println(queenboard);
+  QueenBoard unsolvable = new QueenBoard(2);
+  System.out.println(unsolvable.solve());
+  System.out.println(unsolvable);
+
   }
 
 }
