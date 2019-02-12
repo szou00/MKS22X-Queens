cdd669623e3b6681103210cc8381a6bb3e83021f
Sharon Zou <szou00@stuy.edu>
2019-02-11 19:55:14 -0500

    countSolutions and countAll working

--- QueenBoard.java
+++ QueenBoard.java
@@ -6,31 +6,31 @@ public class QueenBoard {
   }
 
   public boolean addQueen(int r, int c) {
-    if (c == board.length || r == board.length) {
+    if (c == board.length || r == board.length) { //if it's out of bounds, return false
       return false;
     }
     if (board[r][c] == 0) { //if it's a good spot that's not in danger
-      board[r][c] = 5; //place the queen there
+      board[r][c] = -1; //place the queen there
       int d = r; //this will come in use for the diagonal
       int d1 = r; //for the up diagonal
       for (int x = c+1; x < board.length; x++) {
         board[r][x] += 1; //sets the whole row to 1
         if (d < board.length-1) {
           d+=1;
-          board[d][x] += 1; //sets diagonals to 1
+          board[d][x] += 1; //sets the whole down diagonal to 1
         }
         if (d1 > 0) {
           d1-=1;
-          board[d1][x] +=1;
+          board[d1][x] +=1; //sets the whole up diagonal to 1
         }
       }
-      return true;
+      return true; //if it goes through everything, return true
     }
-    return false;
+    return false; //otherwise return false
   }
 
   public boolean removeQueen(int r, int c) {
-    if (board[r][c] == 5) { //if there's a queen there
+    if (board[r][c] == -1) { //if there's a queen there
       board[r][c] = 0; //remove the queen
       int d = r; //this will come in use for the diagonal
       int d1 = r; //for the up diagonal
@@ -49,6 +49,7 @@ public class QueenBoard {
     }
     return false;
   }
+
 
   public void clear() {
     for (int i = 0; i<board.length; i++) {
@@ -76,7 +77,12 @@ public class QueenBoard {
     String ans = "";
     for (int r = 0; r<board.length; r++) {
       for (int c = 0; c<board[0].length; c++) {
-        ans += board[r][c] + " ";
+        if (board[r][c] == -1) {
+          ans+= "Q ";
+        }
+        else {
+          ans+="_ ";
+        }
       }
       ans += "\n";
     }
@@ -94,14 +100,14 @@ public class QueenBoard {
 
   */
   public boolean solve() {
-    return solve(0,0,0);
+    return solve(0,0);
   }
 
-  public boolean solve(int r, int c, int n){
+  public boolean solve(int r, int c){
     //r keeps track of the row
     //c keeps track of column
     //n keeps track of number of queens
-    if (n == board.length) {
+    if (c == board.length) {
       return true;
     }
     if (r == board.length || c == board.length) {
@@ -109,7 +115,7 @@ public class QueenBoard {
     }
     for (int i = 0; i<board.length; i++) {
       if (addQueen(i,c)) {
-        if (solve(i,c+1,n+1)) {
+        if (solve(i,c+1)) {
           return true;
         }
         removeQueen(i,c);
@@ -118,17 +124,17 @@ public class QueenBoard {
     return false;
   }
 
-  public int solveAll(int r, int c, int n){
+  public int solveAll(int r, int c){
     //r keeps track of the row
     //c keeps track of column
     //n keeps track of number of queens
-    if (n >= board.length) {
+    int answer = 0;
+    if (c == board.length) {
       return 1;
     }
-    int answer = 0;
     for (int i = 0; i<board.length; i++) {
       if (addQueen(i,c)) {
-        answer+=solveAll(i,c+1,n+1);
+        answer+=solveAll(i,c+1);
         removeQueen(i,c);
       }
     }
@@ -140,7 +146,7 @@ public class QueenBoard {
   *@throws IllegalStateException when the board starts with any non-zero value
   */
   public int countSolutions(){
-    int answer = solveAll(0,0,0);
+    int answer = solveAll(0,0);
     return answer;
   }
 
